b61d68af53b85d976881e12fab837deec610ba6c
ong37 <47644631+ong37@users.noreply.github.com>
2021-02-23 16:09:28 -0600

    Update create.sql
    fixed column names

--- PP1/create.sql
+++ PP1/create.sql
@@ -5,7 +5,6 @@ DROP TABLE IF EXISTS Category;
 
 CREATE TABLE Item(
     ItemID INTEGER not null, 
-    UserID VARCHAR(255) not null,
     Name VARCHAR(255) not null,
     Currently VARCHAR(255) not null,
     Buy_Price VARCHAR(255),
@@ -13,6 +12,7 @@ CREATE TABLE Item(
     Number_of_Bids DECIMAL not null,
     Started VARCHAR(255) not null,
     Ends TIME not null,
+    UserID VARCHAR(255) not null,
     Description TEXT not null,
     PRIMARY KEY(ItemID),
     FOREIGN KEY(UserID) REFERENCES Person(UserID)
@@ -27,8 +27,8 @@ CREATE TABLE Person(
 );
 
 CREATE TABLE Bid(
-    UserID VARCHAR(255) not null, 
     ItemID INTEGER not null,
+    UserID VARCHAR(255) not null,     
     Time DATETIME not null,
     Amount VARCHAR(255),
     -- PRIMARY KEY(UserID),
